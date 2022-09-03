c04070e838d01931f6c1f4f3b92b655492f893a7
milix90 <76456489+milix90@users.noreply.github.com>
2022-09-03 19:36:39 +0430

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 
-docker exec -it client_api sh -c "cp .env.example .env"
 docker exec -it admin_consume sh -c "cp .env.example .env"
+docker exec -it client_api sh -c "cp .env.example .env"
+
+docker exec -it admin_consume sh -c "composer install"
+docker exec -it admin_consume sh -c "php artisan migrate"
 
 docker exec -it client_api sh -c "composer install"
 docker exec -it client_api sh -c "php artisan migrate"
 docker exec -it client_api sh -c "php artisan db:seed"
 docker exec -it client_api sh -c "php artisan release:check"
-
-docker exec -it admin_consume sh -c "composer install"
-docker exec -it admin_consume sh -c "php artisan migrate"
